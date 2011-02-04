c4427453d953973c99f92773d44330d91a3a067c
Rob Campbell <rcampbell@mozilla.com>
2011-02-04 12:29:29 -0400

    updated constant names and install.rdf, AMO release

--- content/browser.js
+++ content/browser.js
@@ -1,7 +1,7 @@
-const WS_WINDOW_URL = "chrome://workspace/content/workspace.xul";
-const WS_WINDOW_FEATURES = "chrome,titlebar,toolbar,centerscreen,resizable,dialog=no";
+const WORKSPACE_WINDOW_URL = "chrome://workspace/content/workspace.xul";
+const WORKSPACE_WINDOW_FEATURES = "chrome,titlebar,toolbar,centerscreen,resizable,dialog=no";
 
 function openWorkspace() {
-  Services.ww.openWindow(null, WS_WINDOW_URL, "_blank",
-                         WS_WINDOW_FEATURES, null);
+  Services.ww.openWindow(null, WORKSPACE_WINDOW_URL, "_blank",
+                         WORKSPACE_WINDOW_FEATURES, null);
 }
